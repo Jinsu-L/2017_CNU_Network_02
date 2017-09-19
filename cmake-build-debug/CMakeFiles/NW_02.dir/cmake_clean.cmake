df8415180424d6921b73b9424a36153089e5dfff
JINSU <dla0830l@gmail.com>
2017-09-19 17:31:27 +0900

    "push"

---
+++ cmake-build-debug/CMakeFiles/NW_02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NW_02.dir/main.cpp.o"
+  "NW_02.pdb"
+  "NW_02"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NW_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
